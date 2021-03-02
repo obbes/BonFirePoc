1198cb12744a80de7fefd8536fc8364d749a2b86
Bobby <obbes240@gmail.com>
2021-03-02 14:05:59 -0500

    generation

---
+++ backend/bonfirepoc/src/main/java/dev/b2/bonfirepoc/BonfirepocApplication.java
@@ -0,0 +1,13 @@
+package dev.b2.bonfirepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonfirepocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonfirepocApplication.class, args);
+	}
+
+}
---
+++ backend/bonfirepoc/src/test/java/dev/b2/bonfirepoc/BonfirepocApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.b2.bonfirepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonfirepocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
